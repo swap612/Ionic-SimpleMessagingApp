d45b27a5e4d967a42941f80b60895f2283b40e9d
Swapnil Raykar <swap612@gmail.com>
2018-10-04 00:48:18 +0530

    updated home.ts
    Added the logic for sending sms.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Option } from 'ionic-angular';
+import {SMS, SmsOptions} from '@ionic-native/sms';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ phoneNumber : number;
+ textMessage : string;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private sms: SMS) {
+   
   }
+ 
+// Send a text message using default options
+sendTextMessage(){
+  //option var used to stores the sms options 
+  const options: SmsOptions ={
+    replaceLineBreaks:false
+  }
+  //send() from SMS 
+  this.sms.send(String(this.phoneNumber),this.textMessage,options).then((result)=>{
+    console.log(result);
+  },()=>{
+    console.log();
+  })
+}
 
 }
